d1ee2592eb1ce68d9477f6641dfc70770dc6bc94
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-02-03 19:36:05 -0500

    Writing the review routes, controllers and views

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,10 +1,15 @@
 Rainforest::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  get "users/new"
-  get "users/create"
-  resources :products 
+  get "reviews/show"
+  get "reviews/new"
+  get "reviews/edit"
+  # get "sessions/new"
+  # get "sessions/create"
+  # get "sessions/destroy"
+  # get "users/new"
+  # get "users/create"
+  resources :products do 
+    resources :reviews, :except => [:index]
+  end 
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
